d8d1588fb2ed5d9230e26a986222737043825892
Imisi Bailey <imisibailey@gmail.com>
2025-08-25 23:58:21 +0100

    Created 3a

--- exercises/exercise3/3a.js
+++ exercises/exercise3/3a.js
@@ -3,5 +3,4 @@ import  dayjs  from "https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js"
 
 const day = dayjs();
 
-const date = day.format('MMMM D')
-console.log(date)
+console.log(day.format('MMMM D'))
