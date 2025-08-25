f997593864155e9a16d14613c8b84706e8293963
Imisi Bailey <imisibailey@gmail.com>
2025-08-26 00:20:56 +0100

    Created 3e

---
+++ exercises/exercise3/3e.js
@@ -0,0 +1,23 @@
+import  dayjs  from "https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js"
+
+const day = dayjs();
+
+
+let date = day.format('dddd')
+
+function isWeekend(date) {
+  if(date === 'Saturday') {
+    console.log('It is weekend');
+  } else if (date === 'Sunday'){
+    console.log('It is weekend')
+  } else if (date === 'sunday'){
+    console.log('It is weekend')
+  } else if (date === 'Saturday'){
+    console.log('It is weekend')
+  } else {
+    console.log(date)
+  }
+}
+
+isWeekend(date);
+
