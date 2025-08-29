7c37917349cbcf4b7b8dccdf104e73d20b2c0187
Imisi Bailey <imisibailey@gmail.com>
2025-08-29 13:27:22 +0100

    Create automated test for addToCart

--- jasmine-standalone-5.1.1/data/cartTest.js
+++ jasmine-standalone-5.1.1/data/cartTest.js
@@ -2,8 +2,22 @@ import {addToCart, cart, loadFromStorage} from '../../data/cart.js'
 
 describe('test suite: addToCart', () => {
   it('adds an existing product to the cart', () => {
-
-
+    spyOn(localStorage, 'setItem');
+    
+    spyOn(localStorage, 'getItem').and.callFake(() => {
+      return JSON.stringify([{
+        productId: 'id2',
+        quantity: 1,
+        deliveryOptionId: '1'
+      }]);
+    });
+    loadFromStorage();
+  
+    addToCart('id2');
+    expect(cart.length).toEqual(1);
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+    expect(cart[0].productId).toEqual('id2');
+    expect(cart[0].quantity).toEqual(2)
   });
 
   it('add a new product to the cart', () => {
@@ -18,6 +32,6 @@ describe('test suite: addToCart', () => {
     expect(cart.length).toEqual(1);
     expect(localStorage.setItem).toHaveBeenCalledTimes(1);
     expect(cart[0].productId).toEqual('id2');
-    expect(cart[0].quantity.toEqual(1))
+    expect(cart[0].quantity).toEqual(1)
   })
 })
