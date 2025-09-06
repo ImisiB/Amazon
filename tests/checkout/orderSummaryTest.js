72fda3a70862b90664deccf36259b7aff4d2b908
Imisi Bailey <imisibailey@gmail.com>
2025-09-06 18:18:32 +0100

    Load products in beforeAll hook

--- tests/checkout/orderSummaryTest.js
+++ tests/checkout/orderSummaryTest.js
@@ -1,10 +1,17 @@
 import {renderOrderSummary} from '../../scripts/checkout/orderSummary.js';
 import {loadFromStorage, cart} from '../../data/cart.js'
+import { loadProducts } from '../../data/products.js';
 
 describe('test suite: renderOrderSummary', () => {
   const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6'
   const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d'
   
+  beforeAll((done) => {
+    loadProducts(() => {
+      done();
+    });
+  });
+
   beforeEach(() => {
     spyOn(localStorage, 'setItem');
 
@@ -30,9 +37,10 @@ describe('test suite: renderOrderSummary', () => {
   })
 
   it('displays the cart', () => {
+    const id = document.querySelector('.js-cart-item-container')
     expect(
-      document.querySelector('.js-cart-item-container').length
-    ).toEqual(2);
+      document.querySelector('.js-cart-item-container').innerText
+    ).toContain(2);
     expect(
       document.querySelector(`.js-product-quantity-${productId1}`).innerText
     ).toContain('Quantity: 2')
