c4aca8193e36725d4435c7c1de40bcd4a77dbe87
Imisi Bailey <imisibailey@gmail.com>
2025-09-08 14:42:53 +0100

    Create order using the backend

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -2,6 +2,7 @@ import { cart } from "../../data/cart.js";
 import { getProduct } from "../../data/products.js";
 import { getDeliveryOption } from "../../data/deliveryOptions.js";
 import formatCurrency from "../utils/money.js";
+import { addOrder } from "../../data/orders.js";
 
 export function updateCart() {
   let cartQuantity = 0;
@@ -16,19 +17,19 @@ export function updateCart() {
     return cartQuantity;
 }
 
-
 export function renderPaymentSummary() {
   let productPriceCents = 0;
 
   let shippingPriceCents = 0;
 
-  cart.forEach(cartItem => {
+  cart.forEach((cartItem) => {
     const product = getProduct(cartItem.productId);
     productPriceCents += product.priceCents * cartItem.quantity;
 
     const deliveryOption = getDeliveryOption(cartItem.deliveryOptionId)
 
     shippingPriceCents += deliveryOption.priceCents
+  });
 
     const totalBeforeTaxCents = productPriceCents + shippingPriceCents;
     const taxCents = totalBeforeTaxCents * 0.1;
@@ -75,13 +76,33 @@ export function renderPaymentSummary() {
         <div class="js-payment-summary-money payment-summary-money">$${formatCurrency(totalCents)}</div>
       </div>
 
-      <button class="place-order-button button-primary">
+      <button class="place-order-button button-primary js-place-order">
         Place your order
       </button>
     `;
+
   document.querySelector('.js-payment-summary')
     .innerHTML = paymentSummaryHTML;
 
-  });
+document.querySelector('.js-place-order')
+  .addEventListener('click', async () => {
+    try {
+      const response = await fetch('https://supersimplebackend.dev/orders', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          cart: cart
+        })
+      });
 
+      const order = await response.json();
+      addOrder(order);
+    } catch (error) {
+      console.log('Unexpected error. Please try again later.');
+    }
+
+    window.location.href = "orders.html";
+  });
 }
